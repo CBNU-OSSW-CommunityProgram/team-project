e3f7252bbf8b4513e5dd3ff0e94d1cfe44a951a3
동동이 <drdragon777@naver.com>
2021-08-25 15:22:15 +0900

    [Refactor] : (#1) - framework 키워드 삭제

--- src/data.js
+++ src/data.js
@@ -1,14 +1,7 @@
 import {fundamentalsCollection} from "./data/fundamentals.js";
 import {pwaCollection} from "./data/pwa.js";
-import {architectureCollection} from "./data/architecture-and-paradigmes.js";
 import {buildtoolsCollection} from "./data/build-tools.js";
-import {testingCollection} from "./data/testing.js";
-import {frameworksAndLibrariesCollection} from "./data/frameworks-and-libraries.js";
-import {modernCollection} from "./data/modern.js";
-import {teamCollaborationCollection} from "./data/team-collaboration.js";
-import {algorithmsCollection} from "./data/algorithms.js";
 import {designAndUXCollection} from "./data/design-and-ux.js";
-import {databasesCollection} from "./data/databases.js";
 
 /**
  * All available collections.
@@ -17,12 +10,5 @@ export const collections = [
 	fundamentalsCollection,
 	pwaCollection,
 	buildtoolsCollection,
-	frameworksAndLibrariesCollection,
-	testingCollection,
-	architectureCollection,
-	teamCollaborationCollection,
 	designAndUXCollection,
-	modernCollection,
-	algorithmsCollection,
-	databasesCollection,
 ];
