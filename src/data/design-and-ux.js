334e74bab95ab86bb9c79d3e92d1b8e873d822ea
quddnr <quddnr217@naver.com>
2021-09-06 22:33:24 +0900

    [Feat] : (#40) - 블록체인 설명 변경,추가

--- src/data/design-and-ux.js
+++ src/data/design-and-ux.js
@@ -6,26 +6,30 @@ export const designAndUXCollection = {
 				{
 					name: "explanation",
 					description: {
-						text: "Learn about color theory and how you can make your own color schemes.",
+						text: "블록체인 설명",
 						links: [
-							["All you need to know about colors in UI Design", "https://uxdesign.cc/all-you-need-to-know-about-colors-in-ui-design-theory-practice-235179712522"],
-							["The Role of Color in UX", "https://www.toptal.com/designers/ux/color-in-ux"],
-							["10 Questions of Color Theory", "https://medium.muz.li/10-questions-of-color-theory-that-will-make-you-a-better-ux-designer-b5244c1341a6"],
-							["Wikipedia - Color Theory", "https://en.wikipedia.org/wiki/Color_theory"],
-							["The Meaning of Color", "https://www.smashingmagazine.com/2010/01/color-theory-for-designers-part-1-the-meaning-of-color/"],
-							["50 Gorgeous Color Schemes", "https://visme.co/blog/website-color-schemes/"],
+							[
+								"블록체인",
+								"https://ko.wikipedia.org/wiki/%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8",
+							 ],
+							
 						]
 					},
 					skills: [
 						{
 							name: "block-chain",
 							description: {
-								text: "Learn how to use the color wheel when making a color scheme.",
+								text: "블록체인은 대규모의 노드들 사이에서 각 노드에 분산 저장된 장부의 데이터를 항상 있도록 하는 합의 수렴 알고리즘으로 볼 수 있다. 이러한 능력은 노드가 익명으로 실행되거나, 연결이 좋지 않거나, 심지어 신뢰할 수 없는 운영자가 참여하는 것도 가능하게 한다",
 								links: [
-									["Color Theory: Brief Guide For Designers", "https://uxplanet.org/color-theory-brief-guide-for-designers-76e11c57eaa"],
-									["Color theory and the color wheel", "https://www.canva.com/colors/color-wheel/"],
-									["The fundamentals of understanding color theory", "https://99designs.dk/blog/tips/the-7-step-guide-to-understanding-color-theory/"],
-									["Adobe - Color wheel", "https://color.adobe.com/"],
+									[
+										"블록체인 개념",
+										"https://www.markany.com/kr/portfolio-posts/%EC%9D%80%EA%B7%BC-%EC%9E%98-%EB%AA%A8%EB%A5%B4%EB%8A%94-%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8-%EA%B0%9C%EB%85%90/",
+									 ],
+									 [
+										"블록체인 기술이란?",
+										"https://www.ibm.com/kr-ko/topics/what-is-blockchain",
+									 ],
+									
 								]
 							},
 						}
@@ -34,19 +38,17 @@ export const designAndUXCollection = {
 				{
 					name: "Decentral",
 					description: {
-						text: "Learn about typography and how to describe fonts.",
+						text: "일반적인 장부에는 수표나 영수증 또는 약속어음의 교환내역이 기록되는 반면에, 블록체인은 그것 자체가 거래장부인 동시에 거래증서(수표, 영수증, 약속어음)이다. 비트코인에서는 거래들의 지불되지 않은 결과의 형태로 존재한다고 표현한다",
 						links: [
-							["Google - Understanding typography", "https://material.io/design/typography/understanding-typography.html#type-classification"],
-							["How to Understand Typography", "https://www.howtogeek.com/howto/30065/how-to-understand-typography-like-a-professional-designer/"],
-							["Typography Elements Everyone Needs to Understand", "https://medium.com/gravitdesigner/typography-elements-everyone-needs-to-understand-5fdea82f470d"],
-							["Youtube - 10 rules to help you rule type", "https://www.youtube.com/watch?v=QrNi9FmdlxY"],
-							["Font Design", "https://www.canva.com/learn/font-design/"],
-							["Typography Tutorial for Beginners", "https://blog.hubspot.com/marketing/typography-terms-introduction"],
-							["A Crash Course in Typography", "https://www.jotform.com/blog/a-crash-course-in-typography-the-basics-of-type/"],
-							["Type Classifications", "https://www.fonts.com/content/learning/fontology/level-1/type-anatomy/type-classifications"],
-							["Google Fonts", "https://fonts.google.com/"],
-							["7 web typography rules", "https://uxdesign.cc/7-web-typography-rules-27de68c60f6"],
-							["15 Best Practices To Excellent UI", "https://medium.muz.li/typography-in-mobile-design-15-best-practices-to-excellent-ui-5eaf18280ad"],
+							[
+								"탈중앙화의 진짜의미",
+								"https://medium.com/hashed-kr/decentralization-that-matters-kr-5b6f4ff5ef74",
+							 ],
+							 [
+								"블록체인 탈중앙화?",
+								"https://www.coindeskkorea.com/news/articleView.html?idxno=74802",
+							 ],
+							
 						]
 					},
 				
@@ -56,25 +58,26 @@ export const designAndUXCollection = {
 						{
 							name: "Open-type",
 							description: {
-								text: "Learn about font sizes and how to choose the right one.",
+								text: "블록체인망의 장점은 불량한 사용자로부터의 보안을 요하지 않으며 접근 제어가 필요없다는 점이다.[10] 즉, 전송 계층으로서 블록체인을 사용하여 다른 곳의 신뢰나 승인 없이 애플리케이션을 네트워크에 추가할 수 있음",
 								links: [
-									["Type scale", "https://type-scale.com/"],
-									["The importance of font size", "https://www.cardinalpath.com/the-importance-of-font-size/"],
-									["Website font size: Size does matter", "https://www.liveseysolar.com/website-font-size-size-does-matter/"],
-									["CSS Tricks - font-size", "https://css-tricks.com/almanac/properties/f/font-size/"],
-									["CSS Tricks - Fluid Typography", "https://css-tricks.com/snippets/css/fluid-typography/"],
+									[
+										"구글링",
+										"https://www.google.com/search?q=%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8+%EA%B0%9C%EB%B0%A9%ED%98%95&oq=%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8+%EA%B0%9C%EB%B0%A9%ED%98%95&aqs=chrome..69i57j0i30j0i8i30j0i67i395j0i20i131i263i395i433i512j0i67i395j0i20i263i395i512j0i67i395j0i512j0i131i395i433i512.2189j1j15&sourceid=chrome&ie=UTF-8",
+									 ],
+									
 								]
 							},
 						},
 						{
 							name: "Permission-type",
 							description: {
-								text: "Learn about line spacing and how to choose the right one.",
+								text: "허가형 블록체인은 접근 제어 계층을 사용하여 네트워크 접근자를 관리한다.",
 								links: [
-									["Best UX practices for line spacing", "https://www.justinmind.com/blog/best-ux-practices-for-line-spacing/"],
-									["Line spacing", "https://practicaltypography.com/line-spacing.html"],
-									["Why you should go big with line spacing", "https://www.invisionapp.com/inside-design/line-spacing/"],
-									["CSS Tricks - line-height", "https://css-tricks.com/almanac/properties/l/line-height/"],
+									[
+										"암호화폐는 허가형이 필수적인가?!?",
+										"https://www.newstof.com/news/articleView.html?idxno=10220",
+									 ],
+								
 								]
 							},
 						}
@@ -83,64 +86,81 @@ export const designAndUXCollection = {
 				{
 					name: "Application-field",
 					description: {
-						text: "Learn how the C.R.A.P. design principles can help you point out the qualities (or flaws) in a design.",
+						text: "적용분야",
 						links: [
-							["Build Better User Experience", "https://vwo.com/blog/crap-design-principles/"],
-							["CRAP Principles", "https://www.instructables.com/id/CRAP-principles/"],
-							["The Non-Designer's Design Book", "https://www.amazon.com/Non-Designers-Design-Book-4th/dp/0133966151"],
-							["Youtube - The Non-Designers Design Book", "https://www.youtube.com/watch?v=NZ3ryLUR8CI"],
-							["How CRAP Will Make You a Better Designer", "https://creativemarket.com/blog/how-crap-will-make-you-a-better-designer"],
-							["The Big Four: Contrast, Repetition, Alignment, Proximity", "https://www.presentationzen.com/chapter6_spread.pdf"],
+							[
+								"학술자료",
+								"https://scholar.google.co.kr/scholar?q=%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8+%EC%A0%81%EC%9A%A9%EB%B6%84%EC%95%BC&hl=ko&as_sdt=0&as_vis=1&oi=scholart",
+							 ],
+							
 						]
 					},
 					skills: [
 						{
 							name: "certification",
 							description: {
-								text: "Learn how the principles of contrast can help you when designing.",
+								text: "블록체인을 이용하여 인증을 강화 할 수 있습니다.",
 								links: [
-									["Designing with contrast: 20 tips from a designer", "https://www.canva.com/learn/contrasting-colors/"],
-									["Contrast Design principle of Graphic Design", "https://www.youtube.com/watch?v=r0SAPEw0ru4"],
-									["Contrast In Design", "https://www.designcontest.com/blog/contrast-in-design/"],
-									["Fully understanding contrast in design", "https://www.webdesignerdepot.com/2010/09/fully-understanding-contrast-in-design/"],
+									[
+										"블록체인을 이용한 인증강화?",
+										"https://www.acronis.com/ko-kr/technology/blockchain-notary/",
+									 ],
+									 [
+										"공인인증서 대체를 위한 블록체인 기반 개인인증 방안 연구",
+										"https://scienceon.kisti.re.kr/srch/selectPORSrchArticle.do?cn=NPAP12685840&dbt=NPAP",
+									 ],
+								
 								]
 							},
 						},
 						{
 							name: "Payment-and-remittance",
 							description: {
-								text: "Learn how the principles of repetition can help you when designing.",
+								text: "국내 은행등등 결제 시스템에 도입이 됩니다.",
 								links: [
-									["Repetition Design principle of Graphic Design", "https://www.youtube.com/watch?v=u55BW6WBqVU"],
-									["Repetition Principle of Design", "https://254-online.com/repetition-principle-of-design/"],
-									["Repetition, Pattern, and Rhythm", "https://www.interaction-design.org/literature/article/repetition-pattern-and-rhythm"],
-									["Repetition, principles of design", "https://www.johnlovett.com/repetition"],
-									["How to Use Repetition Effectively", "https://www.edgee.net/the-principles-of-graphic-design-how-to-use-repetition-effectively/"],
+									[
+										"금융권 블록체인 사업 본격화…결제·송금부터 인증까지",
+										"https://zdnet.co.kr/view/?no=20190626181738",
+									 ],
+									 [
+										"[블록체인&위크]결제·송금 시장 문제 해결, 블록체인에서 찾는다",
+										"https://signalm.sedaily.com/NewsView/1VGRKO9Y6Y/GZ0304",
+									 ],
+									
 								]
 							},
 						},
 						{
 							name: "Securities-transaction",
 							description: {
-								text: "Learn how the principles of alignment can help you when designing.",
+								text: "거래 가능한 주식,증권 등등 블록체인으로 인해 증가했습니다.",
 								links: [
-									["Alignment Design principle of Graphic Design", "https://www.youtube.com/watch?v=2kokQi9vb6E"],
-									["Alignment Principle Of Design", "https://254-online.com/alignment-principle-design/"],
-									["The Art Of Alignment In Graphic Design", "https://www.designmantic.com/blog/art-of-alignment-graphic-design/"],
-									["Introduction to Alignment", "https://uxengineer.com/principles-of-design/alignment/"],
+									[
+										"블록체인 기반 스마트 주식 거래 시스템의 설계 및 구현 ",
+										"https://scienceon.kisti.re.kr/srch/selectPORSrchArticle.do?cn=DIKO0014373244&dbt=DIKO",
+									 ],
+									 [
+										"거래 가능한 비상장주식, 블록체인으로 확 늘린다",
+										"https://zdnet.co.kr/view/?no=20200819162719",
+									 ],
+									
 								]
 							},
 						},
 						{
 							name: "Smart-Contract-Loan-Investment-Trading",
 							description: {
-								text: "Learn how the principles of proximity can help you when designing.",
+								text: "국제 무역에서도 블록체인을 황용한 가능성을 연구중이며 발전 가능성이 무궁무진하다 합니다.",
 								links: [
-									["Proximity Design principle of Graphic Design", "https://www.youtube.com/watch?v=xUdqSiI_G8Y"],
-									["How to Use Proximity Effectively", "https://www.edgee.net/the-principles-of-graphic-design-how-to-use-proximity-effectively/"],
-									["Proximity Principle Of Design", "https://254-online.com/proximity-principle-design/"],
-									["Gestalt Theory for UX Design: Principle of Proximity", "https://uxplanet.org/gestalt-theory-for-ux-design-principle-of-proximity-e56b136d52d1"],
-									["Principles of Design: Proximity", "https://www.sitepoint.com/principles-of-design-proximity/"],
+									[
+										"블록체인기반 스마트계약은 무엇인가? 스마트 계약이 이끌 금융업 혁신적 변화의 가치",
+										"https://www.samsungsds.com/kr/insights/IR_blockchain_smartcontract.html",
+									 ],
+									 [
+										"블록체인 비즈니스 사례 1. 스마트계약,송금,사물인터넷",
+										"https://www.markany.com/kr/portfolio-posts/%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8-%EB%B9%84%EC%A6%88%EB%8B%88%EC%8A%A4-%EC%82%AC%EB%A1%80-1/",
+									 ],
+									
 								]
 							},
 						}
@@ -149,72 +169,77 @@ export const designAndUXCollection = {
 				{
 					name: "Bitcoin",
 					description: {
-						text: "Learn why consistency is one of the most important building-blocks in a design.",
+						text: "비트코인",
 						links: [
-							["Design principle: Consistency", "https://uxdesign.cc/design-principle-consistency-6b0cf7e7339f"],
-							["Consistency, A Key Design Principle", "https://blog.prototypr.io/consistency-a-key-design-principle-5d125469da8e"],
-							["Consistency: MORE than what you think", "https://www.interaction-design.org/literature/article/consistency-more-than-what-you-think"],
-							["Principle of Consistency in User Interface Design", "https://www.interaction-design.org/literature/article/principle-of-consistency-and-standards-in-user-interface-design"],
-							["The value of consistent design", "https://www.invisionapp.com/inside-design/consistent-design/"],
+								[
+									"사회 속에서 유용하게 쓰이는 블록체인 기술을 만들고 싶다",
+									"https://www.docdocdoc.co.kr/news/articleView.html?idxno=2010583",
+								 ],
+								 [
+									"블록체인과 비트코인(가상화폐)의 법적 문제",
+									"https://www.kci.go.kr/kciportal/ci/sereArticleSearch/ciSereArtiView.kci?sereArticleSearchBean.artiId=ART002351608",
+								 ],
 						]
 					},
 				},
 				{
 					name: "Digital-Note",
 					description: {
-						text: "Learn how to use space in your design.",
+						text: "블록체인 위에 인스턴트 메신저, 블록체인 작업 증명에 기반을 둔 뱅킹 예금 시스템",
 						links: [
-							["Space in Design Systems", "https://medium.com/eightshapes-llc/space-in-design-systems-188bcbae0d62"],
-							["How to use spacing margin and spacing in design", "https://visme.co/blog/how-to-properly-use-margins-and-spacing-in-design/"],
-							["How To Use Space In Design", "https://vanseodesign.com/web-design/design-space/"],
-							["Importance of White Space in Design", "https://blog.prototypr.io/importance-of-white-space-in-design-5a40c0e65bfd"],
-							["Negative Space in Design", "https://tubikstudio.com/negative-space-in-design-tips-and-best-practices-2/"],
-							["How to Change Line Spacing in InDesign", "https://www.shutterstock.com/support/article/how-to-change-line-spacing-in-indesign"],
+							[
+								"디지털노트XDN",
+								"https://gencms.tistory.com/entry/%EB%94%94%EC%A7%80%ED%84%B8%EB%85%B8%ED%8A%B8XDN",
+							 ],
+							 [
+								"디지털 노트 코인 - 비트코인 같은 화폐를 목표로",
+								"https://cjsdnjs.tistory.com/669",
+							 ],
+						
 						]
 					},
 				},
 				{
 					name: "Namecoin",
 					description: {
-						text: "Learn how to improve the user experience by handling errors in an intuitive way.",
+						text: "블록체인에 데이터 저장 기능 제공.",
 						links: [
-							["Design an Error Handling System before you structure your App", "https://medium.com/lattice-product-development/design-an-error-handling-system-before-you-structure-your-app-or-software-project-part-i-efe6a1674628"],
-							["UI Error Handling", "https://uimovement.com/tag/error-handling/"],
-							["Creating Error Messages", "https://uxdesign.cc/creating-error-messages-best-practice-in-ux-design-cda3be0f5e16"],
+							[
+								"블록체인 비교연구: 비트코인·네임코인·메디블록",
+								"https://coinone.co.kr/exchange/trade/btc/krw?__cf_chl_jschl_tk__=pmd_1.G4xgBaNt_HOigahQZEyDgSQ9o2WjOcB_jL7jnmSf0-1630932472-0-gqNtZGzNAdCjcnBszQil",
+							 ],
+							 [
+								"블록체인 도메인 과 네임코인",
+								"https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=aepkoreanet&logNo=221283136331",
+							 ],
+							
 						]
 					},
 				},
 				{
 					name: "Mastercoin",
 					description: {
-						text: "Learn how a loading state can improve the user experience.",
+						text: "다양한 거래를 처리 가능한 블록체인",
 						links: [
-							["Everything you need to know about skeleton screens", "https://uxdesign.cc/what-you-should-know-about-skeleton-screens-a820c45a571a"],
-							["Engaging users with progressive loading in skeleton screen", "https://uxdesign.cc/engaging-users-with-progressive-loading-in-skeleton-screen-335a4e287a55"],
-							["Design and code responsive Skeleton Screens", "https://marinaaisa.com/blog/design-and-code-skeletons-screens/"],
-							["How to Speed Up Your UX with Skeleton Screens", "https://www.sitepoint.com/how-to-speed-up-your-ux-with-skeleton-screens/"],
-							["A Bone to Pick with Skeleton Screens", "https://www.viget.com/articles/a-bone-to-pick-with-skeleton-screens/"],
-							["When to use loaders & empty states", "https://uxdesign.cc/when-to-use-loaders-empty-states-ebd23cecc7d6"],
-							["Empty/Loading State", "https://www.uisources.com/pattern/empty-loading-state"],
-							["When You Need to Show a Button’s Loading State", "https://uxmovement.com/buttons/when-you-need-to-show-a-buttons-loading-state/"],
-							["Stop Using Loading Spinner", "https://blog.iamsuleiman.com/stop-using-loading-spinner-theres-something-better/"],
+							[
+								"마스터(MASTER)코인에 대해 알아보자",
+								"https://blog.naver.com/vngltsz/222371647932",
+							 ],
+							 
 						]
 					},
 				},
 				{
 					name: "Peercoin",
 					description: {
-						text: "Learn how the golden ratio can help you when choose measurements for your design.",
+						text: "작업증명의 대안으로 지분증명 추가.",
 						links: [
-							["The Golden Ratio in Design", "https://designshack.net/articles/layouts/golden-ratio-in-design/"],
-							["A guide to the Golden Ratio for designers", "https://www.invisionapp.com/inside-design/golden-ratio-designers/"],
-							["What is the golden ratio", "https://www.canva.com/learn/what-is-the-golden-ratio/"],
-							["The Golden Ratio: A designer's guide", "https://www.creativebloq.com/design/designers-guide-golden-ratio-12121546"],
-							["Golden Ratio: What It Is And Why Should You Use It In Design", "https://blog.prototypr.io/golden-ratio-what-it-is-and-why-should-you-use-it-in-design-7c3f43bcf98"],
-							["Golden Ratio Typography (GRT) Calculator", "https://grtcalculator.com/"],
-							["How to Use the Golden Ratio", "https://www.companyfolders.com/blog/golden-ratio-design-examples"],
-							["Golden Ratio in Design", "https://wpamelia.com/golden-ratio/"],
-							["How To Use The Golden Ratio In Design", "https://www.format.com/magazine/resources/design/golden-ratio-in-design"],
+							
+							 [
+								"구글링",
+								"https://www.google.com/search?q=%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8+%ED%94%BC%EC%96%B4%EC%BD%94%EC%9D%B8&sxsrf=AOaemvLCNe-yUk2tEz-ZllZ83hQhuvvoVg%3A1630935080903&ei=KBg2YdzRNrqEr7wPi_S_6Aw&oq=%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8+%ED%94%BC%EC%96%B4%EC%BD%94%EC%9D%B8&gs_lcp=Cgdnd3Mtd2l6EAMyBQghEKABOgQIIxAnOgYIABAIEB46BQgAEIAEOgoIABCABBCHAhAUSgQIQRgBUJJAWPBVYNFWaAFwAHgAgAGQAYgBpAySAQQwLjEzmAEAoAEBwAEB&sclient=gws-wiz&ved=0ahUKEwjchZjRuuryAhU6wosBHQv6D80Q4dUDCA4&uact=5",
+							 ],
+						
 						]
 					},
 				},
