7724002ab557e9105ee992cc537ab1b2b3f82bc5
동동이 <drdragon777@naver.com>
2021-09-06 20:36:28 +0900

    [Cho] : (#48) - AI file name change

--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
-import {fundamentalsCollection} from "./data/fundamentals.js";
-import {pwaCollection} from "./data/pwa.js";
-import {virtualrealityCollection} from "./data/virtual-reality.js";
-import {designAndUXCollection} from "./data/design-and-ux.js";
+import { artificialintelligence } from "./data/artificial-intelligence.js";
+import { pwaCollection } from "./data/pwa.js";
+import { virtualrealityCollection } from "./data/virtual-reality.js";
+import { designAndUXCollection } from "./data/design-and-ux.js";
 
 /**
  * All available collections.
  */
 export const collections = [
-	fundamentalsCollection,
-	pwaCollection,
-	virtualrealityCollection,
-	designAndUXCollection,
+  artificialintelligence,
+  pwaCollection,
+  virtualrealityCollection,
+  designAndUXCollection,
 ];
