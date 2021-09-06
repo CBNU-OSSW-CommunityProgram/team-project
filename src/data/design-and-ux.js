3d8129d83688326e5e47777efc27c9d6effbf28f
quddnr <quddnr217@naver.com>
2021-09-06 22:55:50 +0900

    push

--- src/data/design-and-ux.js
+++ src/data/design-and-ux.js
@@ -6,7 +6,6 @@ export const designAndUXCollection = {
 				{
 					name: "explanation",
 					description: {
-<<<<<<< HEAD
 						text: "블록체인 설명",
 						links: [
 							[
@@ -14,18 +13,12 @@ export const designAndUXCollection = {
 								"https://ko.wikipedia.org/wiki/%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8",
 							 ],
 							
-=======
-						text: "Learn about typography and how to describe fonts.",
-						links: [
-							,
->>>>>>> 7aa5b7703c61b545129061244d06585c9573ceed
 						]
 					},
 					skills: [
 						{
 							name: "block-chain",
 							description: {
-<<<<<<< HEAD
 								text: "블록체인은 대규모의 노드들 사이에서 각 노드에 분산 저장된 장부의 데이터를 항상 있도록 하는 합의 수렴 알고리즘으로 볼 수 있다. 이러한 능력은 노드가 익명으로 실행되거나, 연결이 좋지 않거나, 심지어 신뢰할 수 없는 운영자가 참여하는 것도 가능하게 한다",
 								links: [
 									[
@@ -37,11 +30,6 @@ export const designAndUXCollection = {
 										"https://www.ibm.com/kr-ko/topics/what-is-blockchain",
 									 ],
 									
-=======
-								text: "Learn about typography and how to describe fonts.",
-								links: [
-									,
->>>>>>> 7aa5b7703c61b545129061244d06585c9573ceed
 								]
 							},
 						}
@@ -52,7 +40,6 @@ export const designAndUXCollection = {
 					description: {
 						text: "일반적인 장부에는 수표나 영수증 또는 약속어음의 교환내역이 기록되는 반면에, 블록체인은 그것 자체가 거래장부인 동시에 거래증서(수표, 영수증, 약속어음)이다. 비트코인에서는 거래들의 지불되지 않은 결과의 형태로 존재한다고 표현한다",
 						links: [
-<<<<<<< HEAD
 							[
 								"탈중앙화의 진짜의미",
 								"https://medium.com/hashed-kr/decentralization-that-matters-kr-5b6f4ff5ef74",
@@ -61,8 +48,6 @@ export const designAndUXCollection = {
 								"블록체인 탈중앙화?",
 								"https://www.coindeskkorea.com/news/articleView.html?idxno=74802",
 							 ],
-=======
->>>>>>> 7aa5b7703c61b545129061244d06585c9573ceed
 							
 						]
 					},
@@ -75,13 +60,10 @@ export const designAndUXCollection = {
 							description: {
 								text: "블록체인망의 장점은 불량한 사용자로부터의 보안을 요하지 않으며 접근 제어가 필요없다는 점이다.[10] 즉, 전송 계층으로서 블록체인을 사용하여 다른 곳의 신뢰나 승인 없이 애플리케이션을 네트워크에 추가할 수 있음",
 								links: [
-<<<<<<< HEAD
 									[
 										"구글링",
 										"https://www.google.com/search?q=%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8+%EA%B0%9C%EB%B0%A9%ED%98%95&oq=%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8+%EA%B0%9C%EB%B0%A9%ED%98%95&aqs=chrome..69i57j0i30j0i8i30j0i67i395j0i20i131i263i395i433i512j0i67i395j0i20i263i395i512j0i67i395j0i512j0i131i395i433i512.2189j1j15&sourceid=chrome&ie=UTF-8",
 									 ],
-=======
->>>>>>> 7aa5b7703c61b545129061244d06585c9573ceed
 									
 								]
 							},
@@ -91,15 +73,11 @@ export const designAndUXCollection = {
 							description: {
 								text: "허가형 블록체인은 접근 제어 계층을 사용하여 네트워크 접근자를 관리한다.",
 								links: [
-<<<<<<< HEAD
 									[
 										"암호화폐는 허가형이 필수적인가?!?",
 										"https://www.newstof.com/news/articleView.html?idxno=10220",
 									 ],
 								
-=======
-									
->>>>>>> 7aa5b7703c61b545129061244d06585c9573ceed
 								]
 							},
 						}
@@ -110,13 +88,10 @@ export const designAndUXCollection = {
 					description: {
 						text: "적용분야",
 						links: [
-<<<<<<< HEAD
 							[
 								"학술자료",
 								"https://scholar.google.co.kr/scholar?q=%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8+%EC%A0%81%EC%9A%A9%EB%B6%84%EC%95%BC&hl=ko&as_sdt=0&as_vis=1&oi=scholart",
 							 ],
-=======
->>>>>>> 7aa5b7703c61b545129061244d06585c9573ceed
 							
 						]
 					},
@@ -126,7 +101,6 @@ export const designAndUXCollection = {
 							description: {
 								text: "블록체인을 이용하여 인증을 강화 할 수 있습니다.",
 								links: [
-<<<<<<< HEAD
 									[
 										"블록체인을 이용한 인증강화?",
 										"https://www.acronis.com/ko-kr/technology/blockchain-notary/",
@@ -136,9 +110,6 @@ export const designAndUXCollection = {
 										"https://scienceon.kisti.re.kr/srch/selectPORSrchArticle.do?cn=NPAP12685840&dbt=NPAP",
 									 ],
 								
-=======
-									
->>>>>>> 7aa5b7703c61b545129061244d06585c9573ceed
 								]
 							},
 						},
@@ -147,7 +118,6 @@ export const designAndUXCollection = {
 							description: {
 								text: "국내 은행등등 결제 시스템에 도입이 됩니다.",
 								links: [
-<<<<<<< HEAD
 									[
 										"금융권 블록체인 사업 본격화…결제·송금부터 인증까지",
 										"https://zdnet.co.kr/view/?no=20190626181738",
@@ -156,8 +126,6 @@ export const designAndUXCollection = {
 										"[블록체인&위크]결제·송금 시장 문제 해결, 블록체인에서 찾는다",
 										"https://signalm.sedaily.com/NewsView/1VGRKO9Y6Y/GZ0304",
 									 ],
-=======
->>>>>>> 7aa5b7703c61b545129061244d06585c9573ceed
 									
 								]
 							},
@@ -167,7 +135,6 @@ export const designAndUXCollection = {
 							description: {
 								text: "거래 가능한 주식,증권 등등 블록체인으로 인해 증가했습니다.",
 								links: [
-<<<<<<< HEAD
 									[
 										"블록체인 기반 스마트 주식 거래 시스템의 설계 및 구현 ",
 										"https://scienceon.kisti.re.kr/srch/selectPORSrchArticle.do?cn=DIKO0014373244&dbt=DIKO",
@@ -176,8 +143,6 @@ export const designAndUXCollection = {
 										"거래 가능한 비상장주식, 블록체인으로 확 늘린다",
 										"https://zdnet.co.kr/view/?no=20200819162719",
 									 ],
-=======
->>>>>>> 7aa5b7703c61b545129061244d06585c9573ceed
 									
 								]
 							},
@@ -187,7 +152,6 @@ export const designAndUXCollection = {
 							description: {
 								text: "국제 무역에서도 블록체인을 황용한 가능성을 연구중이며 발전 가능성이 무궁무진하다 합니다.",
 								links: [
-<<<<<<< HEAD
 									[
 										"블록체인기반 스마트계약은 무엇인가? 스마트 계약이 이끌 금융업 혁신적 변화의 가치",
 										"https://www.samsungsds.com/kr/insights/IR_blockchain_smartcontract.html",
@@ -196,8 +160,6 @@ export const designAndUXCollection = {
 										"블록체인 비즈니스 사례 1. 스마트계약,송금,사물인터넷",
 										"https://www.markany.com/kr/portfolio-posts/%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8-%EB%B9%84%EC%A6%88%EB%8B%88%EC%8A%A4-%EC%82%AC%EB%A1%80-1/",
 									 ],
-=======
->>>>>>> 7aa5b7703c61b545129061244d06585c9573ceed
 									
 								]
 							},
@@ -209,7 +171,6 @@ export const designAndUXCollection = {
 					description: {
 						text: "비트코인",
 						links: [
-<<<<<<< HEAD
 								[
 									"사회 속에서 유용하게 쓰이는 블록체인 기술을 만들고 싶다",
 									"https://www.docdocdoc.co.kr/news/articleView.html?idxno=2010583",
@@ -218,9 +179,6 @@ export const designAndUXCollection = {
 									"블록체인과 비트코인(가상화폐)의 법적 문제",
 									"https://www.kci.go.kr/kciportal/ci/sereArticleSearch/ciSereArtiView.kci?sereArticleSearchBean.artiId=ART002351608",
 								 ],
-=======
-							
->>>>>>> 7aa5b7703c61b545129061244d06585c9573ceed
 						]
 					},
 				},
@@ -229,7 +187,6 @@ export const designAndUXCollection = {
 					description: {
 						text: "블록체인 위에 인스턴트 메신저, 블록체인 작업 증명에 기반을 둔 뱅킹 예금 시스템",
 						links: [
-<<<<<<< HEAD
 							[
 								"디지털노트XDN",
 								"https://gencms.tistory.com/entry/%EB%94%94%EC%A7%80%ED%84%B8%EB%85%B8%ED%8A%B8XDN",
@@ -239,9 +196,6 @@ export const designAndUXCollection = {
 								"https://cjsdnjs.tistory.com/669",
 							 ],
 						
-=======
-							
->>>>>>> 7aa5b7703c61b545129061244d06585c9573ceed
 						]
 					},
 				},
@@ -250,7 +204,6 @@ export const designAndUXCollection = {
 					description: {
 						text: "블록체인에 데이터 저장 기능 제공.",
 						links: [
-<<<<<<< HEAD
 							[
 								"블록체인 비교연구: 비트코인·네임코인·메디블록",
 								"https://coinone.co.kr/exchange/trade/btc/krw?__cf_chl_jschl_tk__=pmd_1.G4xgBaNt_HOigahQZEyDgSQ9o2WjOcB_jL7jnmSf0-1630932472-0-gqNtZGzNAdCjcnBszQil",
@@ -260,9 +213,6 @@ export const designAndUXCollection = {
 								"https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=aepkoreanet&logNo=221283136331",
 							 ],
 							
-=======
-						
->>>>>>> 7aa5b7703c61b545129061244d06585c9573ceed
 						]
 					},
 				},
@@ -271,15 +221,11 @@ export const designAndUXCollection = {
 					description: {
 						text: "다양한 거래를 처리 가능한 블록체인",
 						links: [
-<<<<<<< HEAD
 							[
 								"마스터(MASTER)코인에 대해 알아보자",
 								"https://blog.naver.com/vngltsz/222371647932",
 							 ],
 							 
-=======
-							
->>>>>>> 7aa5b7703c61b545129061244d06585c9573ceed
 						]
 					},
 				},
@@ -289,14 +235,11 @@ export const designAndUXCollection = {
 						text: "작업증명의 대안으로 지분증명 추가.",
 						links: [
 							
-<<<<<<< HEAD
 							 [
 								"구글링",
 								"https://www.google.com/search?q=%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8+%ED%94%BC%EC%96%B4%EC%BD%94%EC%9D%B8&sxsrf=AOaemvLCNe-yUk2tEz-ZllZ83hQhuvvoVg%3A1630935080903&ei=KBg2YdzRNrqEr7wPi_S_6Aw&oq=%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8+%ED%94%BC%EC%96%B4%EC%BD%94%EC%9D%B8&gs_lcp=Cgdnd3Mtd2l6EAMyBQghEKABOgQIIxAnOgYIABAIEB46BQgAEIAEOgoIABCABBCHAhAUSgQIQRgBUJJAWPBVYNFWaAFwAHgAgAGQAYgBpAySAQQwLjEzmAEAoAEBwAEB&sclient=gws-wiz&ved=0ahUKEwjchZjRuuryAhU6wosBHQv6D80Q4dUDCA4&uact=5",
 							 ],
 						
-=======
->>>>>>> 7aa5b7703c61b545129061244d06585c9573ceed
 						]
 					},
 				},
